3bfde85bec420a0e3f9e4ef827961910366d2931
moaz basheer <moazbasheer97@gmail.com>
2020-12-27 18:03:25 +0200

    add notification engine

---
+++ Sprint 2/notification-api/src/main/java/org/notify/notification_api/application/NotificationEngine.java
@@ -0,0 +1,20 @@
+package Sprint_2;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class NotificationEngine {
+    protected static ArrayList<Notification>smsNotification=new ArrayList<>();
+    protected static ArrayList<Notification>mailNotification=new ArrayList<>();
+    protected static ArrayList<Notification>notifications=new ArrayList<>();
+    //to be implemented to send notification 
+    /*public void Dequeue()
+    {
+	   Notification n = new SmsNotification();
+	   n.getSendway();
+    }*/
+}
